06b15bafd5f03696b5db5c8a46d63d0f9464b177
ahmed-marwan <atmarwan@gmail.com>
2021-08-04 18:26:22 +0200

    Create input component and implement auto-complete feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,76 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Papa from 'papaparse';
+import Input from './components/Input';
+import './styles/App.scss';
+
+export interface Product {
+  title: string;
+  gtin: number;
+  gender: string;
+  sale_price: string;
+  price: string;
+  image_link: string;
+  additional_image_link: string;
+}
 
 function App() {
-  // Parsing CSV data
-  Papa.parse('data/test.csv', {
-    header: true,
-    download: true,
-    step: function (row) {
-      console.log('Row: ', row.data);
-    },
-    complete: function () {
-      console.log('Finished');
-    },
-  });
+  const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState<Product[]>([]);
+  const firstUpdate = useRef(true);
 
-  return <div></div>;
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Sanitizing user input
+    const searchTerm = e.target.value.trim().toLocaleLowerCase();
+    setInputValue(searchTerm);
+  };
+
+  useEffect(() => {
+    // Preventing unecessary data parsing data on 1st render
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+
+    let searchResults: Product[] = [];
+
+    // Parsing CSV data
+    Papa.parse('data/test.csv', {
+      header: true,
+      download: true,
+      //@@TODO fix type any
+      step: function (row: any) {
+        if (inputValue.length) {
+          const product = row.data;
+
+          if (product.title.toLocaleLowerCase().includes(inputValue)) {
+            searchResults.push(product);
+          }
+        }
+      },
+      complete: function () {
+        // Limiting auto-suggested products shown to user to 10
+        setSuggestions(searchResults.slice(0, 10));
+      },
+    });
+  }, [inputValue]);
+
+  // Populating input field with the suggested product that user clicks on
+  const selectSuggestion = (suggestion: string) => {
+    setInputValue(suggestion);
+    setSuggestions([]);
+  };
+
+  return (
+    <main>
+      <Input
+        inputValue={inputValue}
+        handleOnChange={handleOnChange}
+        suggestions={suggestions}
+        selectSuggestion={selectSuggestion}
+        setInputValue={setInputValue}
+      />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Input.tsx
@@ -0,0 +1,80 @@
+import React, { useRef } from 'react';
+import { Product } from '../App';
+import '../styles/Input.scss';
+
+interface InputProps {
+  inputValue: string;
+  handleOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  suggestions: Product[];
+  selectSuggestion: (suggestion: string) => void;
+  setInputValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function Input({
+  inputValue,
+  handleOnChange,
+  suggestions,
+  selectSuggestion,
+  setInputValue,
+}: InputProps) {
+  const renderSuggestions = () => {
+    if (suggestions.length) {
+      return (
+        <ul>
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion.gtin}
+              onClick={() => selectSuggestion(suggestion.title)}
+            >
+              {suggestion.title}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  if (inputRef.current) {
+    inputRef.current.focus();
+  }
+
+  return (
+    <div className="input-container">
+      <div>
+        <input
+          type="text"
+          title="Search"
+          autoComplete="off"
+          autoCorrect="off"
+          spellCheck="false"
+          placeholder="Search"
+          autoFocus
+          value={inputValue}
+          onChange={handleOnChange}
+          ref={inputRef}
+        />
+        {inputValue && (
+          <svg
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="25px"
+            className="icon"
+            onClick={() => setInputValue('')}
+          >
+            <title>Clear</title>
+            <path
+              fill="#444A59"
+              d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+            ></path>
+          </svg>
+        )}
+      </div>
+      {renderSuggestions()}
+    </div>
+  );
+}
+
+export default Input;
