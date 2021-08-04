39dfa6402fa4d399733b264a080016ce8e01b7dd
ahmed-marwan <atmarwan@gmail.com>
2021-08-04 10:01:31 +0200

    Parse csv data file using Papa Parse npm package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Papa from 'papaparse';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Parsing CSV data
+  Papa.parse('data/test.csv', {
+    header: true,
+    download: true,
+    step: function (row) {
+      console.log('Row: ', row.data);
+    },
+    complete: function () {
+      console.log('Finished');
+    },
+  });
+
+  return <div></div>;
 }
 
 export default App;
